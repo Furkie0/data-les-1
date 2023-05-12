aeed402663126cca77ce226f4f17b4830b86a6fa
Furkie0 <133210182+Furkie0@users.noreply.github.com>
2023-05-12 14:34:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$int1 = 12;
+$int2 = 10;
+$sum = $int1 + $int2;
+echo $sum;
+
+
+$int1 = 12;
+$int2 = 10;
+$product = $int1 * $int2;
+echo $product;
+
+
+$naam = "Furkan";
+$zin = "Welkom Furkan";
+echo $zin;
+?>
+
+<?php
+
+$mijnNaam = "Carl";
+$jouwNaam = "Camel";
+echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+?>
